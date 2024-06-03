d269c20735a223356c17218b5d665f9964730550
AdamJaworski <jaworskiadam0203@gmail.com>
2024-06-03 14:30:48 +0200

    measure_time update

--- utilities.py
+++ utilities.py
@@ -5,7 +5,5 @@ def measure_time(func):
     def wrapper(*args):
         start = time()
         func(args[0])
-        total_time = (time() - start)
-        #print(f"Execution time: {total_time:.4f}s\n")
-        return total_time
+        return time() - start
     return wrapper
